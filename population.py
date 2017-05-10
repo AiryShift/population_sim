619cd1de146a5110f50a7cb60ce8cf7b692453c5
AiryShift <religionofaperturelabs@gmail.com>
2017-05-10 11:40:38 +1000

    Add argparser for parameters

--- population.py
+++ population.py
@@ -1,10 +1,8 @@
+import argparse
 import random
 import sys
 import time
 from collections import Counter
-
-NUM_PEOPLE = 100
-NUM_GENERATIONS = 100
 
 
 def decision(probability):
@@ -17,22 +15,23 @@ def next_generation(people):
     for i in range(0, len(people) // 2 * 2, 2):
         first, second = people[i], people[i + 1]
         # probability for same named children
-        if decision(0.55):
+        if decision(args.same_name_chance):
             new_surname = random.choice([first, second])
             people[i] = people[i + 1] = new_surname
 
         # probability for new child
-        if decision(0.2):
+        # multiplies by 2 because only couples produce extra children
+        if decision((args.growth_factor - 1) * 2):
             new.append(random.choice([first, second]))
     people.extend(new)
 
 
-if __name__ == '__main__':
+def main(args):
     start_time = time.time()
 
-    people = list(range(NUM_PEOPLE))
-    for generation in range(NUM_GENERATIONS):
-        print('Current generation: {}'.format(generation), file=sys.stderr)
+    people = list(range(args.people))
+    for generation in range(args.generations):
+        print('Current generation:', generation, file=sys.stderr)
         next_generation(people)
 
     people_frequency = Counter(people)
@@ -42,3 +41,13 @@ if __name__ == '__main__':
     print('Total population:', sum(people_frequency.values()))
 
     print('Took {:.2f}s'.format(time.time() - start_time), file=sys.stderr)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Does some population counting')
+    parser.add_argument('-p', '--people', type=int, default=100, help='Number of people')
+    parser.add_argument('-g', '--generations', type=int, default=120, help='Number of generations')
+    parser.add_argument('-s', '--same-name-chance', type=float, default=0.55, help='Chance that the two children of a couple have the same last name')
+    parser.add_argument('-f', '--growth-factor', type=float, default=1.1, help='Average growth factor of the population')
+    args = parser.parse_args()
+    main(args)
